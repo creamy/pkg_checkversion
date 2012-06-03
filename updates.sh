f3acf4311d8de3a622a99cd3e9f0d0d530427599
Waitman Gobble <waitman@waitman.net>
2012-06-03 11:59:03 -0700

    run after updating system

--- updates.sh
+++ updates.sh
@@ -1,25 +1,7 @@
-pkg_delete --force ImageMagick-6.7.5.10
-pkg_add ImageMagick-6.7.6.9.tbz
-pkg_delete --force autoconf-2.68
-pkg_add autoconf-2.69.tbz
-pkg_delete --force automake-1.11.1
-pkg_add automake-1.4.6_6.tbz
-pkg_delete --force avahi-app-0.6.29_2
-pkg_add avahi-app-0.6.29_3.tbz
-pkg_delete --force bash-4.2.20
-pkg_add bash-4.2.28.tbz
-pkg_delete --force boost-libs-1.45.0_2
-pkg_add boost-libs-1.48.0.tbz
-pkg_delete --force ca_root_nss-3.13.3
-pkg_add ca_root_nss-3.13.4.tbz
-pkg_delete --force cairo-1.10.2_3,2
-pkg_add cairo-1.12.2,1.tbz
-pkg_delete --force cantarell-fonts-0.0.6
-pkg_add cantarell-fonts-0.0.8.tbz
-pkg_delete --force cmake-2.8.7
-pkg_add cmake-2.8.8.tbz
-pkg_delete --force dmxproto-2.3
-pkg_add dmxproto-2.3.1.tbz
+pkg_delete --force Thunar-1.3.0
+pkg_add Thunar-1.4.0.tbz
+pkg_delete --force cairo-1.12.2,1
+pkg_add cairo-1.10.2_3,2.tbz
 pkg_delete --force docbook-1.4
 pkg_add docbook-5.0_1.tbz
 pkg_delete --force docbook-4.1_4
@@ -38,166 +20,15 @@ pkg_delete --force docbook-xml-4.3
 pkg_add docbook-xml-4.5.tbz
 pkg_delete --force docbook-xml-4.4_1
 pkg_add docbook-xml-4.5.tbz
-pkg_delete --force dri-7.4.4,2
-pkg_add dri-7.6.1,2.tbz
-pkg_delete --force dri2proto-2.3
-pkg_add dri2proto-2.6.tbz
-pkg_delete --force ffmpeg-0.7.11_4,1
-pkg_add ffmpeg-0.7.12_1,1.tbz
-pkg_delete --force fontconfig-2.8.0_1,1
-pkg_add fontconfig-2.9.0,1.tbz
-pkg_delete --force freetype2-2.4.7
-pkg_add freetype2-2.4.9_1.tbz
-pkg_delete --force fusefs-kmod-0.3.9.p1.20080208_9
-pkg_add fusefs-kmod-0.3.9.p1.20080208_10.tbz
-pkg_delete --force garcon-0.1.10
-pkg_add garcon-0.1.11.tbz
-pkg_delete --force gcc-4.6.4.20120309
-pkg_add gcc-4.8.0.20120513.tbz
-pkg_delete --force gdk-pixbuf-2.23.5_1
-pkg_add gdk-pixbuf-2.23.5_2.tbz
-pkg_delete --force git-1.7.9.6
-pkg_add git-1.7.10.2.tbz
-pkg_delete --force gmp-5.0.4
-pkg_add gmp-5.0.5.tbz
-pkg_delete --force gpac-libgpac-0.4.5_4,1
-pkg_add gpac-libgpac-0.4.5_5,1.tbz
-pkg_delete --force gtk-2.24.6
+pkg_delete --force gcc-4.6.4.20120511
+pkg_add gcc-4.8.0.20120527.tbz
+pkg_delete --force gcc-4.8.0.20120513
+pkg_add gcc-4.8.0.20120527.tbz
+pkg_delete --force glib-1.2.10_13
+pkg_add glib-2.28.8_4.tbz
+pkg_delete --force gtk-1.2.10_22
 pkg_add gtk-3.0.12_1.tbz
-pkg_delete --force hal-0.5.14_18
-pkg_add hal-0.5.14_19.tbz
-pkg_delete --force help2man-1.40.7
-pkg_add help2man-1.40.9.tbz
-pkg_delete --force libGL-7.4.4
-pkg_add libGL-7.6.1.tbz
-pkg_delete --force libGLU-7.4.4
-pkg_add libGLU-7.6.1.tbz
-pkg_delete --force libXaw-1.0.8,2
-pkg_add libXaw-1.0.9,2.tbz
-pkg_delete --force libXi-1.4.3,1
-pkg_add libXi-1.4.5,1.tbz
-pkg_delete --force libXt-1.0.9,1
-pkg_add libXt-1.1.1,1.tbz
-pkg_delete --force libdrm-2.4.12_1
-pkg_add libdrm-2.4.17_1.tbz
-pkg_delete --force libgphoto2-2.4.11_1
-pkg_add libgphoto2-2.4.13.tbz
-pkg_delete --force libiconv-1.13.1_2
-pkg_add libiconv-1.14.tbz
-pkg_delete --force libiodbc-3.52.7
-pkg_add libiodbc-3.52.8.tbz
-pkg_delete --force libpurple-2.10.3
-pkg_add libpurple-2.10.4.tbz
-pkg_delete --force libxml2-2.7.8_2
-pkg_add libxml2-2.7.8_3.tbz
-pkg_delete --force luit-1.1.0
-pkg_add luit-1.1.0_1.tbz
-pkg_delete --force midori-0.4.4
-pkg_add midori-0.4.6.tbz
-pkg_delete --force mousepad-0.2.16_8
-pkg_add mousepad-0.2.16_9.tbz
-pkg_delete --force mpfr-3.1.0_2
-pkg_add mpfr-3.1.0_3.tbz
-pkg_delete --force mplayer-1.0.r20111218_4
-pkg_add mplayer-1.0.r20120322_1.tbz
-pkg_delete --force mysql-client-5.5.21
-pkg_add mysql-client-5.5.24.tbz
-pkg_delete --force p5-Error-0.17017
-pkg_add p5-Error-0.17018.tbz
-pkg_delete --force p5-IO-Socket-SSL-1.59
-pkg_add p5-IO-Socket-SSL-1.74.tbz
-pkg_delete --force p5-Net-SSLeay-1.47
-pkg_add p5-Net-SSLeay-1.48.tbz
-pkg_delete --force p5-URI-1.59
-pkg_add p5-URI-1.60.tbz
-pkg_delete --force pciids-20120227
-pkg_add pciids-20120507.tbz
-pkg_delete --force pcre-8.30_1
-pkg_add pcre-8.30_2.tbz
-#
-#perl-5.8.9_7.tbz +MD5 CHECK NOGO.
-#MD5 (perl-5.8.9_7.tbz) = 862a9710b4ba27e833fd0295210c17b3
-#missing or mismatch check CHECKSUMS.MD5
-#
-pkg_delete --force perl-5.12.4_4
-pkg_add perl-5.8.9_7.tbz
-pkg_delete --force pidgin-2.10.3
-pkg_add pidgin-2.10.4.tbz
-pkg_delete --force png-1.4.8_1
-pkg_add png-1.4.11.tbz
-pkg_delete --force postgresql-client-9.0.7_1
-pkg_add postgresql-client-9.2.b1.tbz
-pkg_delete --force python27-2.7.2_4
-pkg_add python27-2.7.3.tbz
-pkg_delete --force qtcreator-2.4.1
-pkg_add qtcreator-2.5.0.tbz
-pkg_delete --force ristretto-0.3.4
-pkg_add ristretto-0.3.7.tbz
-pkg_delete --force ruby-1.8.7.358,1
-pkg_add ruby-1.9.3.194,1.tbz
-pkg_delete --force samba34-libsmbclient-3.4.14
-pkg_add samba34-libsmbclient-3.4.17.tbz
-pkg_delete --force scintilla-3.0.4
-pkg_add scintilla-3.1.0.tbz
-pkg_delete --force scite-3.0.4
-pkg_add scite-3.1.0.tbz
-pkg_delete --force shared-mime-info-0.90
-pkg_add shared-mime-info-1.0_1.tbz
-pkg_delete --force sqlite3-3.7.10
-pkg_add sqlite3-3.7.12.tbz
-pkg_delete --force sudo-1.8.4_1
-pkg_add sudo-1.8.5.p1.tbz
-pkg_delete --force tcl-8.5.11
-pkg_add tcl-8.6.b.2.tbz
-pkg_delete --force tiff-4.0.0_3
-pkg_add tiff-4.0.1_1.tbz
-pkg_delete --force tk-8.5.11
-pkg_add tk-8.6.b.2.tbz
-pkg_delete --force v4l_compat-1.0.20110720
-pkg_add v4l_compat-1.0.20120501.tbz
-pkg_delete --force vte-0.26.2
-pkg_add vte-0.26.2_1.tbz
-pkg_delete --force x264-0.119.2113
-pkg_add x264-0.123.2189_1.tbz
-pkg_delete --force xdg-utils-1.0.2_6
-pkg_add xdg-utils-1.0.2_7.tbz
-pkg_delete --force xdpyinfo-1.2.0
-pkg_add xdpyinfo-1.3.0.tbz
-pkg_delete --force xdriinfo-1.0.3
-pkg_add xdriinfo-1.0.4.tbz
-pkg_delete --force xf86-input-keyboard-1.5.0
-pkg_add xf86-input-keyboard-1.6.1.tbz
-pkg_delete --force xf86-input-mouse-1.6.0
-pkg_add xf86-input-mouse-1.7.1.tbz
-pkg_delete --force xf86-video-ati-6.14.3
-pkg_add xf86-video-ati-6.14.3_1.tbz
-pkg_delete --force xf86-video-fbdev-0.4.2
-pkg_add xf86-video-fbdev-0.4.2_1.tbz
-pkg_delete --force xf86-video-mach64-6.8.2_1
-pkg_add xf86-video-mach64-6.9.0.tbz
-pkg_delete --force xf86-video-nv-2.1.18
-pkg_add xf86-video-nv-2.1.18_1.tbz
-pkg_delete --force xf86-video-r128-6.8.1_2
-pkg_add xf86-video-r128-6.8.1_3.tbz
-pkg_delete --force xf86-video-radeonhd-1.3.0_4
-pkg_add xf86-video-radeonhd-1.3.0_5.tbz
-pkg_delete --force xf86-video-vesa-2.3.0_1
-pkg_add xf86-video-vesa-2.3.0_2.tbz
-pkg_delete --force xfce4-tumbler-0.1.23
-pkg_add xfce4-tumbler-0.1.24.tbz
-pkg_delete --force xfce4-utils-4.8.3_2
-pkg_add xfce4-utils-4.8.3_3.tbz
-pkg_delete --force xinit-1.3.1,1
-pkg_add xinit-1.3.2,1.tbz
-pkg_delete --force xkeyboard-config-2.1
-pkg_add xkeyboard-config-2.5.1.tbz
-pkg_delete --force xorg-7.5.1
-pkg_add xorg-7.5.2.tbz
-pkg_delete --force xorg-apps-7.5.1
-pkg_add xorg-apps-7.5.2.tbz
-pkg_delete --force xorg-drivers-7.5.1
-pkg_add xorg-drivers-7.5.2.tbz
-pkg_delete --force xorg-server-1.7.7_3,1
-pkg_add xorg-server-1.7.7_5,1.tbz
-pkg_delete --force xterm-278
-pkg_add xterm-279.tbz
+pkg_delete --force gtk-2.24.6_1
+pkg_add gtk-3.0.12_1.tbz
+pkg_delete --force gtk-xfce-engine-2.9.0
+pkg_add gtk-xfce-engine-3.0.0.tbz
