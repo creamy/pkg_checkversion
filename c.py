5e6395ba1ef29c35ab2b5ba79fa66ec0be5036f6
Waitman Gobble <waitman@waitman.net>
2012-06-03 00:20:24 -0700

    download tbz and md5 checksum, write update script

--- c.py
+++ c.py
@@ -13,9 +13,37 @@ import HTMLParser
 import urllib2
 import re
 import subprocess
+import os.path
+import sys
+
+url = "http://ftp.freebsd.org/pub/FreeBSD/ports/amd64/packages/All/"
+
+try:
+	os.unlink('CHECKSUM.MD5')
+except:
+	pass
 
 
-req = urllib2.Request("http://ftp.freebsd.org/pub/FreeBSD/ports/amd64/packages/All/")
+subprocess.Popen(['wget',url+'CHECKSUM.MD5'], stdout=subprocess.PIPE).communicate()[0]
+
+print "Parsing CHECKSUM.MD5. Might take a minute. Please wait....."
+
+chks = {}
+try:
+	f = open('CHECKSUM.MD5', 'r')
+	for line in f:
+		chks[line.strip()] = True
+	f.close()
+except:
+	print "no CHECKSUM.MD5. bail."
+	sys.exit()
+
+print " Done."
+
+print "Downloading list of available packages. Might take a minute. Please wait...."
+
+
+req = urllib2.Request(url)
 res = urllib2.urlopen(req)
 htc = res.read()
 tbz = re.findall(r'href=[\'"]?([^\'" >]+)', htc)
@@ -28,6 +56,14 @@ for t in tbz:
 	rt = '-'.join(l)
 	vers[rt] = vx
 
+print " Done."
+
+try:
+	dun = open('updates.sh','w')
+except:
+	print "cannot open updates.sh for writing. bail."
+	sys.exit()
+
 p = subprocess.Popen(['pkg_info'], stdout=subprocess.PIPE).communicate()[0]
 ln = p.split("\n")
 for l in ln:
@@ -39,4 +75,37 @@ for l in ln:
 	if rt!='':
 		if vers[rt]!=nx:
 			print rt,"\t",nx,"\t",vers[rt]
+			chkfile = rt+'-'+vers[rt]+'.tbz'
+			if (os.path.exists(chkfile)):
+				pass
+			else:
+				subprocess.Popen(['wget',url+rt+'-'+vers[rt]+'.tbz'], stdout=subprocess.PIPE).communicate()[0]
+				
+			ru = subprocess.Popen(['md5',chkfile], stdout=subprocess.PIPE).communicate()[0]
+			dr = ru.split('\n')
+			cu = dr[0].strip()
+
+			try:
+				if chks[cu]:
+					print chkfile," +MD5 CHECK OK"
+				else:
+					chks[cu]='missing or mismatch check CHECKSUMS.MD5'
+					dun.write('#\n')
+					dun.write('#'+chkfile+' +MD5 CHECK NOGO.\n')
+					dun.write('#'+cu+'\n')
+					dun.write('#'+chks[cu]+'\n')
+					dun.write('#\n')
+			except:
+				chks[cu]='missing or mismatch check CHECKSUMS.MD5'
+				dun.write('#\n')
+				dun.write('#'+chkfile+' +MD5 CHECK NOGO.\n')
+				dun.write('#'+cu+'\n')
+				dun.write('#'+chks[cu]+'\n')
+				dun.write('#\n')
+				pass
+
+			dun.write('pkg_delete --force '+rt+'-'+nx+'\n')
+			dun.write('pkg_add '+rt+'-'+vers[rt]+'.tbz\n')
+
+dun.close()
 
